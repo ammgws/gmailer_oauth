aa060532a2472af9ad353c10dd7c94d11c6d7201
Jason <jason@kayoway.com>
2017-10-16 02:24:04 +0900

    Minor changes

--- gmailer_oauth.py
+++ gmailer_oauth.py
@@ -15,7 +15,14 @@ import click
 import requests
 from google_auth import GoogleAuth
 
-APP_NAME = 'gmail-oauth'
+APP_NAME = 'gmailer-oauth'
+
+
+def prepare_message(to, subject, message_text, attachment):
+    if attachment:
+        return create_message_with_attachment(to, subject, message_text, attachment)
+    else:
+        return create_message(to, subject, message_text)
 
 
 def create_message_with_attachment(to, subject, message_text, attachment):
@@ -60,6 +67,12 @@ def create_message(to, subject, message_text):
     return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode('utf8')}
 
 
+def create_dir(ctx, param, directory):
+    if not os.path.isdir(directory):
+        os.makedirs(directory, exist_ok=True)
+    return directory
+
+
 @click.command()
 @click.argument('recipient')
 @click.option(
@@ -78,30 +91,32 @@ def create_message(to, subject, message_text):
     help='Path to attachment.',
 )
 @click.option(
-    '--client-id', '-i',
+    '--client-id',
     type=click.STRING,
     help='Google OAUTH client ID.',
 )
 @click.option(
-    '--client-secret', '-k',
+    '--client-secret',
     type=click.STRING,
     help='Google OAUTH client secret.',
 )
 @click.option(
     '--config-path',
+    type=click.Path(),
     default=os.path.join(os.environ.get('XDG_CONFIG_HOME', os.path.expanduser('~/.config')), APP_NAME),
-    type=click.Path(exists=True),
-    help='Path to directory containing config file.',
+    callback=create_dir,
+    help='Path to directory containing config file. Defaults to XDG config dir.',
 )
 @click.option(
     '--cache-path',
-    type=click.Path(exists=True),
+    type=click.Path(),
     default=os.path.join(os.environ.get('XDG_CACHE_HOME', os.path.expanduser('~/.cache')), APP_NAME),
-    help='Path to directory to store logs and such.',
+    callback=create_dir,
+    help='Path to directory to store logs and such. Defaults to XDG cache dir.',
 )
 @click.option('--dry-run', is_flag=True)
 @click.option('--interactive', '-i', is_flag=True)
-def main(config_path, cache_path, recipient, message, subject, attachment, dry_run, client_id, client_secret):
+def main(config_path, cache_path, recipient, message, subject, dry_run, client_id, client_secret, attachment=None):
     """TODO.
     """
 
@@ -136,27 +151,22 @@ def main(config_path, cache_path, recipient, message, subject, attachment, dry_r
     # Create and send email
     # see https://developers.google.com/gmail/api/guides/uploads
     #     https://developers.google.com/gmail/api/v1/reference/users/messages/send
+    message_body = prepare_message(
+            to=recipient,
+            subject=subject,
+            message_text=message,
+            attachment=attachment,
+    )
     if attachment:
         url = 'https://www.googleapis.com/upload/gmail/v1/users/me/messages/send?uploadType=multipart'
         headers = {
             'Authorization': f'Bearer {oauth.access_token}',
             'Content-Type': 'message/rfc822'
         }
-        message_body = create_message_with_attachment(
-                to=recipient,
-                subject=subject,
-                message_text=message,
-                attachment=attachment,
-        )
         req = requests.Request('POST', url, headers=headers, data=message_body)
     else:
         url = 'https://www.googleapis.com/gmail/v1/users/me/messages/send'
         headers = {'Authorization': f'Bearer {oauth.access_token}'}
-        message_body = create_message(
-                to=recipient,
-                subject=subject,
-                message_text=message,
-        )
         req = requests.Request('POST', url, headers=headers, json=message_body)
 
     req = req.prepare()
